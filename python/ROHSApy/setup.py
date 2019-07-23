04920b981395b8d319f643b43c964ce227e23a82
Antoine Marchal <amarchal@daphyperstars.extra.cea.fr>
2019-07-23 17:45:52 +0000

    update version in setup

--- python/ROHSApy/setup.py
+++ python/ROHSApy/setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
     
 setup(
     name='ROHSApy',
-    version='0.1.7',
+    version='0.1.9',
     description='python interface package for ROHSA software',
     long_description=readme,
     classifiers=[
