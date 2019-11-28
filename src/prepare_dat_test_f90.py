8918a3afd36a37b346a66c0db034826847c09283
antoinemarchal <amarchal@cita.utoronto.ca>
2019-11-28 16:54:00 -0500

    update prepare data

--- src/prepare_dat_test_f90.py
+++ src/prepare_dat_test_f90.py
@@ -7,7 +7,7 @@ from ROHSApy import ROHSA
 import turbulence as tb
 
 #Open data GNILC_IRIS-SFD_adim
-fitsname = "/data/amarchal/SIMUSED/data/intensity_adim.fits"
+fitsname = "/mnt/raid-cita/amarchal/SIMUSED/data/intensity_adim.fits"
 hdu = fits.open(fitsname)
 hdr = hdu[0].header
 cube = hdu[0].data
@@ -16,7 +16,7 @@ NHI = np.ones((1,cube.shape[1],cube.shape[2]))
 rms_cube = np.full((cube.shape[0],cube.shape[1],cube.shape[2]), 1.)
 
 #Open color correction file
-color = fits.open("/data/amarchal/PLANCK/col_cor_iras_hfi_DX9v2_poly.fits")[0].data
+color = fits.open("/home/amarchal/ROHSA/data/col_cor_iras_hfi_DX9v2_poly.fits")[0].data
 
 core_cube = ROHSA(cube)
 core_NHI = ROHSA(NHI)
