868206ad9459fbf3cafcb99425604b705964b0eb
antoinemarchal <amarchal@cita.utoronto.ca>
2019-12-06 15:09:18 -0500

    test simu

--- src/prepare_dat_test_f90.py
+++ src/prepare_dat_test_f90.py
@@ -7,10 +7,10 @@ from ROHSApy import ROHSA
 import marchalib as ml
 
 #Open data GNILC_IRIS-SFD_adim
-fitsname = "/mnt/raid-cita/amarchal/SIMUSED/data/intensity_adim.fits"
+fitsname = "/mnt/raid-cita/amarchal/SIMUSED/data/simu_mamd_sed/intensity_adim_mamd.fits"
 hdu = fits.open(fitsname)
 hdr = hdu[0].header
-cube = hdu[0].data
+cube = hdu[0].data *1.e6
 
 NHI = np.ones((1,cube.shape[1],cube.shape[2]))
 rms_cube = np.full((cube.shape[0],cube.shape[1],cube.shape[2]), 1.)
@@ -27,7 +27,6 @@ core_cube.cube2dat(filename="/home/amarchal/ROHSA/src/SED.dat")
 core_NHI.cube2dat(filename="/home/amarchal/ROHSA/src/NHI.dat")
 core_rms.cube2dat(filename="/home/amarchal/ROHSA/src/rms.dat")
 core_color.rms_map(color, filename="/home/amarchal/ROHSA/src/color.dat")
-
 
 #Open fBm for fft test
 field = fits.open("/mnt/raid-cita/amarchal/DUST/output/fft_fBm.fits")[0].data[0]
